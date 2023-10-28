f8017adcb3bdc497b2192e4a2185e68d978f0f79
François Costa <frankost.costa@gmail.com>
2023-10-28 13:02:15 +0200

    Improve euler_sieve.cpp

--- algorithms/euler_sieve.cpp
+++ algorithms/euler_sieve.cpp
@@ -4,15 +4,15 @@ vector<int> pr;
 
 void linearSieve()  {
     for (int i=2; i <= N; ++i) {
-    if (lp[i] == 0) {
-        lp[i] = i;
-        pr.push_back(i);
-    }
-    for (int j = 0; i * pr[j] <= N; ++j) {
-        lp[i * pr[j]] = pr[j];
-        if (pr[j] == lp[i]) {
-            break;
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; i * pr[j] <= N; ++j) {
+            lp[i * pr[j]] = pr[j];
+            if (pr[j] == lp[i]) {
+                break;
+            }
         }
     }
-}
 }
