7514806f138c54defa2ac44a717778390fa7a480
François Costa <frankost.costa@gmail.com>
2025-05-06 22:47:01 +0200

    Improve dijkstra

--- dijsktra.cpp
+++ dijsktra.cpp
@@ -1,39 +1,52 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <iostream>
 using namespace std;
 
-class Dijkstra {
-    public:
-    Dijksta() {
-     }
+class Dijskstra
+{
+public:
+    Dijskstra(vector<vector<pair<int,int>>> &adj)
+    {
+        this->adj = adj;
+    }
 
-}
+    Dijskstra(vector<vector<pair<int,int>>>&& adj)
+    {
+        this->adj = std::move(adj);
+    }
 
-vector<vector<pair<int, int>>> adj;
-
-void dijkstra(int s, vector<int> & d, vector<int> & p) {
-    const int INF = INT_MAX;
-
-    int n = adj.size();
-    d.assign(n, INF);
-    p.assign(n, -1);
-
-    d[s] = 0;
-    set<pair<int, int>> q;
-    q.insert({0, s});
-    while (!q.empty()) {
-        int v = q.begin()->second;
-        q.erase(q.begin());
-
-        for (auto edge : adj[v]) {
-            int to = edge.first;
-            int len = edge.second;
-
-            if (d[v] + len < d[to]) {
-                q.erase({d[to], to});
-                d[to] = d[v] + len;
-                p[to] = v;
-                q.insert({d[to], to});
+    [[nodiscard]] vector<int> run(int start) {
+        int n = adj.size();
+        vector<int> d(n);
+    
+        d[start] = 0;
+        set<pair<int, int>> q;
+        q.insert({0, start});
+        while (!q.empty())
+        {
+            int v = q.begin()->second;
+            q.erase(q.begin());
+    
+            for (auto edge : adj[v])
+            {
+                int to = edge.first;
+                int len = edge.second;
+    
+                if (d[v] + len < d[to])
+                {
+                    q.erase({d[to], to});
+                    d[to] = d[v] + len;
+                    q.insert({d[to], to});
+                }
             }
         }
+
+        return d;
     }
-}
+    
+private:
+    vector<vector<pair<int,int>>> adj;
+    const long long int INF = LLONG_MAX;
+};
